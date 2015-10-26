a6a2fe93ee12555304c1be11b4ca9272984f9462
bossuet <boargs@gmail.com>
2015-10-26 17:31:32 -0600

    agregado perfil

--- perfil.lua
+++ perfil.lua
@@ -200,11 +200,7 @@ parse:getMe( onGetMe )
         photo.y=_H*0.2
         photo:addEventListener( "touch", menuImg )
         else
-<<<<<<< HEAD
             img1 = display.newImageRect( "img_perfil.png", 180, 180 )
-=======
-            
->>>>>>> 95932ec192281cb38298b3e66a301f020f47bc46
             img1.x = _W*0.5
             img1.y = _H*0.23
             img1:addEventListener( "touch", menuImg )
